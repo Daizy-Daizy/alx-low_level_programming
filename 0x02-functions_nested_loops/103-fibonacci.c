c90426bd374448154b0b3960900f1086b8249427
Daizy-Daizy <chelimodaizzy@gmail.com>
2023-02-22 12:40:37 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1, j = 2, sum = 0, next;
+
+	while (j < 4000000)
+
+	{
+	if (j % 2 == 0)
+	{
+	sum += j;
+	}
+
+	next = i + j;
+	i = j;
+	j = next;
+	}
+
+	 printf("%d\n", sum);
+
+	return (0);
+}
